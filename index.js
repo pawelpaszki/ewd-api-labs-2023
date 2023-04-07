7507345d5262549b6365d1e35f8438f76b0dfe46
Pawel Paszki <pawelpaszki@gmail.com>
2023-04-07 07:08:13 +0100

    feat: add mongoose support for the api

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import genresRouter from './src/genres';
 import createAccountsRouter from './src/accounts/routes';
 import buildDependencies from './src/config/dependencies';
 import createMoviesRouter from './src/movies/routes';
+import db from './src/config/db';
 
 dotenv.config();
+db.init();
 
 const app = express();
 
---
+++ src/config/db.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+export default {
+
+  async init() {
+    if (process.env.DATABASE_DIALECT === "mongo") {
+      // Connect to database
+      mongoose.connect(process.env.DATABASE_URL);
+      const connection = await mongoose.connection;
+
+      connection.on('error', (err) => {
+        console.log(`database connection error: ${err}`);
+      });
+      connection.on('disconnected', () => {
+        console.log('database disconnected');
+      });
+      connection.once('open', async () => {
+        console.log(`database connected to ${connection.name} on ${connection.host}`);
+        //delete the existing  collections if in development mode
+        if (process.env.NODE_ENV == "development") {
+
+          // Get all collections
+          const collections = await connection.db.listCollections().toArray();
+
+          //delete all collections
+          collections
+            .map((collection) => collection.name)
+            .forEach(async (collectionName) => {
+              connection.dropCollection(collectionName);
+            });
+        }
+      });
+    }
+
+  }
+};
