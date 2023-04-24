4722094b4f4ed04ce8490c9204db1f04a09d2c4f
Pawel Paszki <pawelpaszki@gmail.com>
2023-04-24 20:07:15 +0100

    feat: add general error handling to movies endpoints

--- src/genres/controllers/index.js
+++ src/genres/controllers/index.js
@@ -4,23 +4,31 @@ import { validateParams } from "../../utils/paramsValidator";
 export default (dependencies) => {
 
   const getGenre = async (request, response, next) => {
-    //input
-    const id = request.params.genre_id;
-    validateParams(request);
-    // Treatment
-    const genre = await genreService.getGenre(id, dependencies);
-    //output
-    if (genre !== undefined) {
-      response.status(200).json(genre);
-    } else {
-      response.status(404).json({});
+    try {
+      //input
+      const id = request.params.genre_id;
+      validateParams(request);
+      // Treatment
+      const genre = await genreService.getGenre(id, dependencies);
+      //output
+      if (genre !== undefined) {
+        response.status(200).json(genre);
+      } else {
+        response.status(404).json({});
+      }
+    } catch (err) {
+      response.status(500).json({ message: "Failed to get genre" });
     }
   };
   const listGenres = async (request, response, next) => {
-    // Treatment
-    const genres = await genreService.find(dependencies);
-    //output
-    response.status(200).json(genres);
+    try {
+      // Treatment
+      const genres = await genreService.find(dependencies);
+      //output
+      response.status(200).json(genres);
+    } catch (err) {
+      response.status(500).json({ message: "Failed to list genres" });
+    }
   };
 
   return {
--- src/movies/controllers/index.js
+++ src/movies/controllers/index.js
@@ -6,59 +6,95 @@ export default (dependencies) => {
   const getMovie = async (request, response, next) => {
     //input
     const movieId = request.params.resource_id;
-    validateParams(request);
-    // Treatment
-    const movie = await moviesService.getMovie(movieId, dependencies);
-    //output
-    response.status(200).json(movie);
+    try {
+      validateParams(request);
+      // Treatment
+      const movie = await moviesService.getMovie(movieId, dependencies);
+      //output
+      response.status(200).json(movie);
+    } catch (err) {
+      if (err.toString().includes("404")) {
+        response.status(404).json({ message: `movie with id: '${movieId}' not found` });
+      } else {
+        response.status(500).json({ message: "Failed to get movie" });
+      }
+    }
   };
   const find = async (request, response, next) => {
-    //input
-    const query = request.url.substr(request.url.indexOf('?')+1);
-    // Treatment
-    const movies = await moviesService.find(query, dependencies);
-    //output
-    response.status(200).json(movies);
+    try {
+      //input
+      const query = request.url.substr(request.url.indexOf('?') + 1);
+      // Treatment
+      const movies = await moviesService.find(query, dependencies);
+      //output
+      response.status(200).json(movies);
+    } catch (err) {
+      response.status(500).json({ message: "Failed to find movies" });
+    }
   };
 
   const findSimilar = async (request, response, next) => {
     //input
     const movieId = request.params.resource_id;
-    validateParams(request);
-    const query = request.url.substr(request.url.indexOf('?')+1);
-    // Treatment
-    const movies = await moviesService.findSimilar(movieId, query, dependencies);
-    //output
-    response.status(200).json(movies);
+    try {
+      validateParams(request);
+      const query = request.url.substr(request.url.indexOf('?') + 1);
+      // Treatment
+      const movies = await moviesService.findSimilar(movieId, query, dependencies);
+      //output
+      response.status(200).json(movies);
+    } catch (err) {
+      response.status(500).json({ message: `Failed to find movies similar to movie with id: '${movieId}` });
+    }
   };
 
   const getUpcomingMovies = async (request, response, next) => {
-    //input
-    const query = request.url.substr(request.url.indexOf('?')+1);
-    // Treatment
-    const movies = await moviesService.findUpcoming(query, dependencies);
-    //output
-    response.status(200).json(movies);
+    try {
+      //input
+      const query = request.url.substr(request.url.indexOf('?') + 1);
+      // Treatment
+      const movies = await moviesService.findUpcoming(query, dependencies);
+      //output
+      response.status(200).json(movies);
+    } catch (err) {
+      response.status(500).json({ message: `Failed to get upcoming movies` });
+    }
   };
 
   const getMovieImages = async (request, response, next) => {
     //input
     const movieId = request.params.resource_id;
-    // validateParams(request);
-    // Treatment
-    const images = await moviesService.getMovieImages(movieId, dependencies);
-    //output
-    response.status(200).json(images);
+    try {
+      // validateParams(request);
+      // Treatment
+      const images = await moviesService.getMovieImages(movieId, dependencies);
+      //output
+      response.status(200).json(images);
+    } catch (err) {
+      if (err.toString().includes("404")) {
+        response.status(404).json({ message: `movie with id: '${movieId}' not found` });
+      } else {
+        response.status(500).json({ message: "Failed to get movie images" });
+      }
+    }
   };
 
   const getMovieReviews = async (request, response, next) => {
     //input
     const movieId = request.params.resource_id;
-    validateParams(request);
-    // Treatment
-    const images = await moviesService.getMovieReviews(movieId, dependencies);
-    //output
-    response.status(200).json(images);
+    try {
+      validateParams(request);
+      // Treatment
+      const images = await moviesService.getMovieReviews(movieId, dependencies);
+      //output
+      response.status(200).json(images);
+    } catch (err) {
+      if (err.toString().includes("404")) {
+        response.status(404).json({ message: `movie with id: '${movieId}' not found` });
+      } else {
+        response.status(500).json({ message: "Failed to get movie reviews" });
+      }
+    }
   };
 
   return {
