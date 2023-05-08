4ea3266733c260e57941e9375a4cd7176bce5455
Pawel Paszki <pawelpaszki@gmail.com>
2023-05-08 08:33:22 +0100

    chore: add account id logging if present from error handler

--- src/utils/errors/handler.js
+++ src/utils/errors/handler.js
@@ -3,9 +3,10 @@ import { processAndPersistLogs } from "../logProcessor";
 
 function errorHandler(err, req, res, next) {
   const code = (err && err.code) || null;
+  const accountId = (err && err.account_id) || "";
   const error = errorCodes[code] || errorCodes['INTERNAL_ERROR'];
 
-  processAndPersistLogs("error", req, error.statusCode, "");
+  processAndPersistLogs("error", req, error.statusCode, accountId);
 
   return res
     .status(error.statusCode)
